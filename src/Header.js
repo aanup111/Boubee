a0f8028be625e908e67ad7e734572fe72c9f8100
Anup Ghimire <aanup111@gmail.com>
2021-03-18 16:11:59 -0700

    initial header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React from 'react';
 import logo from './logo.png';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 
 function Header() {
@@ -12,18 +15,21 @@ function Header() {
         <div className = 'header_search' >
             <input
             className='header_search' type ='text' />
+            <SearchIcon 
+            className='header_searchIcon' />
         </div>
 
         <div className = 'header_nav'>
             <div className = 'header_nav_user'>
-                Guest
+                Hello Guest
             </div>    
             <div className = 'header_nav_cart'>
-                Cart Icon
+                    <ShoppingCartIcon
+                    className='header_shoppingcartIcon' />
+                  <span className='header_shoppingcartCount'>0</span>
             </div>
-
-
         </div>
+
 
 
 

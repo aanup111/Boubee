a8e109aa8d7b0b93d4765095d7dab36552c33357
Anup Ghimire <aanup111@gmail.com>
2021-04-07 00:31:11 -0700

    revised_products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,36 @@
 import React from 'react'
 import '../css/Home.css'
 import Product from './Product'
-import backgroundImage from '../images/background.PNG'
-const title = 'Rasberry Lip Gloss';
-const image =  backgroundImage;
-const price = 10.99; 
-const rating = 5;
-const id = '1'
+import product1 from '../images/product1.png'
+import product2 from '../images/product2.png'
+import product3 from '../images/product3.png'
+import product4 from '../images/product4.png'
+import product5 from '../images/product5.png'
+import product6 from '../images/product6.png'
+import product7 from '../images/product7.png'
+import product8 from '../images/product8.png'
+import product9 from '../images/product9.png'
 function Home() {
     return (
         <div className='home'>
             <div className='home_container'>
             
            <div className='home_row'>
-                <Product title={title} image={image} price={price} rating={rating} id={id} />
-                
-                   
-            </div>
+                <Product title={'Sony Headphones'} image={product1} price={59.99} rating={4} id={'1'} />
+                <Product title={'Apple Airpods Pro'} image={product9} price={299.99} rating={5} id={'2'} />
+           </div>
 
             <div className='home_row'>
-            <Product title={title} image={image} price={price} rating={rating} />
-            <Product title={title} image={image} price={price} rating={rating} />
-            <Product title={title} image={image} price={price} rating={rating} />  
+            <Product title={'BenQ 27 inch Curved Monitor'} image={product2} price={499.99} rating={4} id={'3'} />
+            <Product title={'Apple Pen'} image={product3} price={110.99} rating={3} id={'4'} />
+            <Product title={'Sony Airbuds'} image={product4} price={49.99} rating={2} id={'5'} />  
             </div>
     
             <div className='home_row'>
-            <Product title={title} image={image} price={price} rating={rating} />
-            <Product title={title} image={image} price={price} rating={rating} />
-            <Product title={title} image={image} price={price} rating={rating} />
-            <Product title={title} image={image} price={price} rating={rating} />
+            <Product title={'White Kitten Plush Toy'} image={product5} price={15.99} rating={5} id={'6'} />
+            <Product title={'Reversible Plush Toy'} image={product6} price={20.99} rating={4} id={'7'} />
+            <Product title={'Mochi Puni Penguin'} image={product7} price={29.99} rating={5} id={'8'} />
+            <Product title={'Mochi Puni Dog'} image={product8} price={25.99} rating={3} id={'9'} />
                 
             </div>
     
--- src/components/Product.js
+++ src/components/Product.js
@@ -26,7 +26,7 @@ function Product({id, title, image, price, rating}) {
     return (
         <div className ='product'>
             <div className ='product_info' >
-                <p>{title}</p>
+                <p className='product_title'>{title}</p>
                 <p className = 'product_price'>
                     <small>$</small>
                     <strong>{price}</strong>
